d4797b05f09d35167144bb9ad415329db0dd010e
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2024-11-06 23:33:54 +0330

    add 003.01.FROM file

---
+++ Course3/SQL Server Scripts/003.01.FROM.sql
@@ -0,0 +1,34 @@
+	
+	USE AdventureWorks2022
+
+	SELECT	ProductCategoryID	FROM	Production.ProductCategory
+
+
+	SELECT	ProductCategoryID	
+	FROM	Production.ProductCategory
+
+	SELECT	ProductCategoryID, Name	
+	FROM	Production.ProductCategory
+
+	SELECT	[ProductCategoryID], [Name]	
+	FROM	[Production].[ProductCategory]
+
+	SELECT					[ProductCategoryID], [Name]	
+	FROM	
+	
+	
+					[Production].[ProductCategory]
+
+
+	SELECT	[ProductCategoryID]
+			,[Name]	
+	FROM	[Production].[ProductCategory]
+
+	SELECT	*
+	FROM	[Production].[ProductCategory]
+
+	SELECT	[ProductCategoryID]
+			,[Name]	
+			,*
+			,[Name]
+	FROM	[Production].[ProductCategory]
