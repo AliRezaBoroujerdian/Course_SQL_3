eb8924a3c19991e4c03dde732f058f01b95576e4
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2024-11-12 13:27:45 +0330

    add alias E.g

---
+++ Course3/SQL Server Scripts/005_Alias_02_E.g.01.sql
@@ -0,0 +1,12 @@
+﻿
+	/*
+		نام و کد دسته‌بندی‌های موجود برای کالا‌ها را نمایش دهید
+		نام ستون‌ها به فارسی نمایش داده‌شود
+	*/
+
+	USE	AdventureWorks2022
+
+
+	SELECT	ProductCategoryID	AS 'کد دسته'
+			,[Name]				AS 'نام دسته'
+	FROM	[Production].[ProductCategory]
