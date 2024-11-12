782c38fc5e753c762c6e4bcfc777194fe62a4742
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2024-11-12 13:04:32 +0330

    add alias file

---
+++ Course3/SQL Server Scripts/005_Alias_01.sql
@@ -0,0 +1,31 @@
+﻿
+	USE	AdventureWorks2022
+
+
+	SELECT	[ProductID]	PID
+	FROM	[Production].[Product]
+
+
+	SELECT	[ProductID]	PID
+			,[Name]		PName
+	FROM	[Production].[Product]
+
+
+	SELECT	[ProductID]	AS PID
+			,[Name]		AS PName
+	FROM	[Production].[Product]
+
+
+	SELECT	[ProductID]	AS 'PID'
+			,[Name]		AS 'PName'
+	FROM	[Production].[Product]
+
+
+	SELECT	[ProductID]	AS 'Product ID'
+			,[Name]		AS 'Product Name'
+	FROM	[Production].[Product]
+
+
+	SELECT	[ProductID]	AS 'کد محصول'
+			,[Name]		AS 'نام محصول'
+	FROM	[Production].[Product]
